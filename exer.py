52f989db7b57cba2dd4ce3e0fbf0a96db9d4496a
kraskoval <kr.muratovski@gmail.com>
2024-11-05 17:24:48 +0200

    Day 25: Creating pdf

---
+++ exer.py
@@ -0,0 +1,17 @@
+from pathlib import Path
+from fpdf import FPDF
+import glob
+
+
+
+
+
+filepaths = glob.glob("Text-Files/*.txt")
+pdf = FPDF(orientation='P', unit='mm', format='A4')
+for filepath in filepaths:
+	filename = Path(filepath).stem
+	pdf.add_page()
+	pdf.set_font('Times' , style='B', size=24)
+	pdf.cell(w=20, h=20, text = filename.capitalize())
+pdf.output('Animals.pdf')
+
--- main.py
+++ main.py
@@ -1,9 +1,21 @@
 import pandas as pd
 import glob
+from fpdf import FPDF
+from pathlib import Path
+# from fpdf.enums import XPos, YPos
 
-filepaths = glob.glob("invoices/*.xlsx")
+
+
+filepaths = glob.glob("invoices/*.xlsx")##
 for path in filepaths:
-	print(path)
-	df = pd.read_excel(path, sheet_name='Sheet 1')
-	print(df)
-	# break
+	# df = pd.read_excel(path, sheet_name='Sheet 1')##
+	filename = Path(path).stem
+	invoice_nr = filename.split('-')[0]
+	pdf = FPDF(orientation='P', unit='mm', format='A4')
+	pdf.add_page()
+	# pdf.set_font()
+	pdf.set_font('Times', style = 'B', size=18)
+	pdf.cell(w=50, h=12, text = f"Invoice nr.{invoice_nr}")
+	# pdf.cell(w=50, h=12, text = "test")
+	pdf.output(f"PDFs/{filename}.pdf")
+
